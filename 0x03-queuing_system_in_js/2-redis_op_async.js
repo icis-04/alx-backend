c9ac38721cfcd501cadda33d68c8a58e1e8f28e5
icis-04 <chidexiwuaku@gmail.com>
2023-03-01 17:46:07 +0000

    redis i ssweet

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,24 @@
+const redis = require('redis');
+
+const {promisify} = require('util');
+
+const client = redis.createClient();
+
+const get = promisify(client.get).bind(client)
+client.on('connect', () => {
+        console.log("Redis client connected to the server");
+});
+
+client.on('error', err => console.log(`Redis client not connected to the server: ${err}`));
+
+function setNewSchool(schoolName, value){
+;	client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName){
+	const res = await get(schoolName);
+	console.log(res);
+};
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
